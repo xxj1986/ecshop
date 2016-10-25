9dbea02ed805d5a0f9aae58a5bce4b883becc2b2
baiyangyang <yangyangbai@dingshengcaifu.cn>
2016-10-25 16:48:19 +0800

    购物车和订单相关接口功能

---
+++ api/order.php
@@ -0,0 +1,221 @@
+<?php
+
+/**
+ * 购物车 相关功能
+ */
+
+
+define('IN_ECS', true);
+
+// 载入初始化文件
+include __DIR__ . '/aip_init.php';
+
+//数据库ORM
+use Illuminate\Database\Capsule\Manager as DB;
+//输入过滤器
+use Illuminate\Http\Request;
+
+//定义认证类
+class Order
+{
+    protected $dev; //设备ID
+    protected $redis;
+
+    public function __construct()
+    {
+        //捕获输入信息
+        $request = Request::capture();
+        //定义错误消息
+        //$this->dev = isset($_REQUEST['device']) ? trim($_REQUEST['device']):function ($res) {$this->response($res);};
+        //连接redis
+        //$redis = new Redis();
+        //$redis->connect(REDIS_HOST, REDIS_PORT);
+        //$this->redis = $redis;
+
+        //参数路由
+        $act = $request->input('act');
+        switch ($act) {
+            case 'set_cart':
+                $this->set_cart($request);
+                break;
+            case 'get_cart_infos':
+                $this->get_cart_infos($request);
+                break;
+            case 'update_cart_infos':
+                $this->update_cart($request);
+                break;
+            case 'delete_cart':
+                $this->delete_cart($request);
+                break;
+            default:
+                $this->response();
+        }
+    }
+
+
+    /*
+     * 获取产品列表信息
+     */
+    private function set_cart(Request $request)
+    {
+        $goods_id = $request->input('goods_id') ? $request->input('goods_id') : 0;
+        $goods_num = $request->input('goods_num') ? $request->input('goods_num') : 0;
+        $user = $request->input('user') ? $request->input('user') : 0;
+
+        if ($goods_id > 0 && $goods_num > 0 && $user > 0) {
+            $goodsInfo = DB::table('goods')
+                ->select('goods_sn', 'goods_name', 'goods_id', 'market_price', 'shop_price')
+                ->where('is_delete', 0)
+                ->where('goods_id', $goods_id)
+                ->first();
+            if ($goodsInfo) {
+                $id = DB::table('cart')->insertGetId(
+                    ['user_id' => $user,
+                        'goods_id' => $goods_id,
+                        'goods_sn' => $goodsInfo->goods_sn,
+                        'goods_name' => $goodsInfo->goods_name,
+                        'market_price' => $goodsInfo->market_price,
+                        'goods_price' => $goodsInfo->shop_price,
+                        'goods_number' => $goods_num,
+                        'is_real' => 1
+                    ]
+                );
+                if ($id) {
+                    $res['errcode'] = '0';
+                    $res['message'] = '添加购物车成功！';
+                    $res['data'] = [];
+                } else {
+                    $res['errcode'] = '10001';
+                    $res['message'] = '添加购物车失败！';
+                    $res['data'] = [];
+                }
+            } else {
+                $res['errcode'] = '10002';
+                $res['message'] = '所选商品信息有误！';
+                $res['data'] = [];
+            }
+        } else {
+            $res['errcode'] = '10003';
+            $res['message'] = '提交参数信息有误！';
+            $res['data'] = [];
+        }
+        $this->response($res);
+    }
+
+    /**
+     * 获取商品信息
+     */
+    private function get_cart_infos(Request $request)
+    {
+        $user = $request->input('user') ? $request->input('user') : 0;
+        if ($user > 0) {
+            $carts = DB::table('cart')
+                ->where('is_real', 1)
+                ->where('user_id', $user)
+                ->select('goods_id','goods_sn','goods_name','goods_number','market_price','goods_price')
+                ->get();
+            //print_r($goods);
+            if ($carts) {
+                $data = [];
+                foreach($carts as $cart){
+                    $data[] = $cart;
+                }
+                if(count($data)>0){
+                    $res['errcode'] = 0;
+                    $res['message'] = '获取购物车信息成功！';
+                }else{
+                    $res['errcode'] = '1001';
+                    $res['message'] = '购物车为空！';
+                }
+                $res ['data'] = $data;
+            } else {
+                $res['errcode'] = '1002';
+                $res['message'] = '获取购物车信息失败！';
+                $res['data'] = [];
+            }
+        } else {
+            $res['errcode'] = '1002';
+            $res['message'] = '参数信息有误！';
+            $res['data'] = [];
+        }
+        $this->response($res);
+    }
+
+    /**
+     * 获取商品对应的属性信息
+     */
+    private function update_cart(Request $request)
+    {
+        $goods_id = $request->input('id') ? $request->input('id') : 0;
+        if ($goods_id > 0) {
+            $attrs = DB::table('goods_attr')
+                ->where('goods_id', $goods_id)
+                ->select('goods_id', 'attr_id', 'attr_value', 'attr_price')
+                ->get();
+            //print_r($goods);
+            if ($attrs) {
+                $res['errcode'] = '0';
+                $res['message'] = '获取商品属性信息成功！';
+                $res['data'] = $attrs;
+            } else {
+                $res['errcode'] = '1001';
+                $res['message'] = '获取商品属性信息失败！';
+                $res['data'] = [];
+            }
+        } else {
+            $res['errcode'] = '1002';
+            $res['message'] = '商品信息有误！';
+            $res['data'] = [];
+        }
+        $this->response($res);
+    }
+
+    /**
+     * 获取商品对应的属性信息
+     */
+    private function delete_cart(Request $request)
+    {
+        $goods_name = $request->input('name') ? $request->input('name') : '红酒';
+
+        $attrs = DB::table('goods')
+            ->where('goods_name', 'like', '%' . $goods_name . '%')//where('name', 'like', 'T%')
+            ->select('goods_id', 'cat_id', 'goods_name', 'goods_number', 'shop_price', 'keywords', 'goods_thumb', 'goods_img', 'last_update')
+            ->orderBy('goods_id', 'ASC')
+            ->take(30)
+            ->get();
+        //print_r($attrs);
+        if ($attrs) {
+            $datas = [];
+            foreach ($attrs as $attr) {
+                $res['errcode'] = '0';
+                $res['message'] = '获取商品属性信息成功！';
+                $data['goods_thumb'] = (!empty($attr->goods_thumb)) ? 'http://' . $_SERVER['SERVER_NAME'] . '/' . $attr->goods_thumb : '';
+                $data['goods_img'] = (!empty($attr->goods_img)) ? 'http://' . $_SERVER['SERVER_NAME'] . '/' . $attr->goods_img : '';
+                $data['shop_price'] = $attr->shop_price;
+                $data['goods_name'] = $attr->goods_name;
+                $data['cat_id'] = $attr->cat_id;
+                $data['goods_number'] = $attr->goods_number;
+                $datas[] = $data;
+            }
+            $res['data'] = $datas;
+        } else {
+            $res['errcode'] = '1001';
+            $res['message'] = '获取商品属性信息失败！';
+            $res['data'] = [];
+        }
+        $this->response($res);
+    }
+
+
+    /*
+     * 输出函数
+     */
+    public function response($res = ['errcode' => 1002, 'message' => 'action not found', 'data' => []])
+    {
+        header('Content-type:text/json');
+        die(json_encode($res));
+    }
+}
+$Order = new Order();
+
+?>
